1d453e71a753165aa1c7a90da916e20382ff753e
Sophia <swoozie14.com>
2017-02-02 21:42:42 -0800

    Add signout functionality and add basic html views

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,12 +2,12 @@ module SessionsHelper
 
   def log_in(user)
     session[:user_id] = user.id
-    current_user(user)
+    @current_user = user
   end
 
-  def log_out(user)
+  def log_out
+    forget(current_user)
     session.delete(:user_id)
-    forget
     @current_user = nil
   end
 
@@ -24,23 +24,19 @@ module SessionsHelper
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
-        current_user(user)
+        @current_user = user
       end
     end
-  end
-
-  def current_user=(user)
-    @current_user = user
   end
 
   def logged_in?
     !current_user.nil?
   end
 
-  def forget
-    forget(current_user)
-    cookies.delete(:remember_token)
+  def forget(user)
+    user.forget
     cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
 end
